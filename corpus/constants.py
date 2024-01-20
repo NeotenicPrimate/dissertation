f78e4c76243242665bf4ff8c1da63c0b66cdc41b
NeotenicPrimate <vladimir.g.borel@gmail.com>
2024-01-19 23:55:47 -0800

    backup

--- corpus/constants.py
+++
@@ -1,8 +0,0 @@
-DATA_PATH = "./data/Subcategory Data/"
-OUTPUT_PATH = './output/'
-CITATION_DF_PATH = './output/citation_dfs/'
-LATEX_TABLE_PATH = "./latex/tables/"
-LATEX_FIGURES_PATH = "./latex/figures/"
-
-OBSIDIAN_IMG_PATH = "/Users/vladimirborel/Documents/Obsidian Vault/Dissertation/Outputs/Images/"
-OBSIDIAN_TABLE_PATH = "/Users/vladimirborel/Documents/Obsidian Vault/Dissertation/Outputs/Tables/"
